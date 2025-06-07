e40f663c2b114317273a86cf2101ff0acca1c6a3
Felipe Galfano <counquer@gmail.com>
2025-06-07 02:55:47 -0400

    chore: agrega configuración mínima moderna de ESLint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      // Puedes agregar reglas personalizadas aquí si lo deseas
+    },
+  },
+];
