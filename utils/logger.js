535be0f20ce9792e5b99e7b55190eee2d4157439
Felipe Galfano <counquer@gmail.com>
2025-06-06 22:00:52 -0400

    refactor: mejora logger y robustez de escritura en NotionService

--- utils/logger.js
+++ utils/logger.js
@@ -1,69 +1,19 @@
-import fs from "fs";
-import path from "path";
+const isDev = process.env.NODE_ENV !== "production";
 
-const ENV = process.env.NODE_ENV || "development";
-const LOG_FILE = process.env.LOG_FILE || path.join(path.dirname(new URL(import.meta.url).pathname), "../logs/app.log");
-
-const COLORS = {
-  reset: "\x1b[0m",
-  gray: "\x1b[90m",
-  cyan: "\x1b[36m",
-  yellow: "\x1b[33m",
-  red: "\x1b[31m",
-  green: "\x1b[32m",
-};
-
-function formatMsg(level, mod, ...msg) {
-  const time = new Date().toISOString();
-  return `[${time}] [${level}] [${mod}] ${msg.join(" ")}`;
-}
-
-function devLogger(level, mod, ...msg) {
-  let prefix = "";
-  switch (level) {
-    case "INFO": prefix = COLORS.cyan; break;
-    case "WARN": prefix = COLORS.yellow; break;
-    case "ERROR": prefix = COLORS.red; break;
-    case "DEBUG": prefix = COLORS.gray; break;
-    default: prefix = COLORS.green;
-  }
-  console.log(prefix + formatMsg(level, mod, ...msg) + COLORS.reset);
-}
-
-function prodLogger(level, mod, ...msg) {
-  if (level === "ERROR" || level === "WARN") {
-    const line = formatMsg(level, mod, ...msg);
-    if (process.env.LOG_TO_FILE === "1") {
-      try {
-        fs.appendFileSync(LOG_FILE, line + "\n");
-      } catch (e) {
-        console.error(line);
-      }
-    } else {
-      console.log(line);
-    }
-  }
+function format(label, msg) {
+  return `[${new Date().toISOString()}] [${label.toUpperCase()}] ${msg}`;
 }
 
 const logger = {
-  info: (mod, ...msg) => {
-    if (ENV === "development") devLogger("INFO", mod, ...msg);
-    else if (ENV === "production") prodLogger("INFO", mod, ...msg);
+  info: (label, ...msgs) => {
+    if (isDev) console.log(format(label, msgs.join(" ")));
   },
-  warn: (mod, ...msg) => {
-    if (ENV === "development") devLogger("WARN", mod, ...msg);
-    else if (ENV === "production") prodLogger("WARN", mod, ...msg);
+  error: (label, ...msgs) => {
+    console.error(format(label, msgs.join(" ")));
   },
-  error: (mod, ...msg) => {
-    if (ENV === "development") devLogger("ERROR", mod, ...msg);
-    else if (ENV === "production") prodLogger("ERROR", mod, ...msg);
+  warn: (label, ...msgs) => {
+    console.warn(format(label, msgs.join(" ")));
   },
-  debug: (mod, ...msg) => {
-    if (ENV === "development") devLogger("DEBUG", mod, ...msg);
-  },
-  _setLogger: (mockLogger) => {
-    Object.assign(logger, mockLogger);
-  }
 };
 
 export default logger;
